76e1d5307859cd96e22c7a3cfd9fbbcfb953dcb7
Karl Santiago <kcsantiago@gmail.com>
2013-09-18 22:12:10 -0400

    Create script for nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+$(function() {
+  var $navMethods = $("#nav-methods"),
+      $navFigures = $("#nav-figures"),
+      $sectionMethods = $("#sections-methods"),
+      $sectionFigures = $("#sections-figures");
+  
+  $navMethods.click(function(event) {
+    $(this).addClass("ui--active");
+    $navFigures.removeClass("ui--active");
+    $sectionMethods.fadeIn();
+    $sectionFigures.fadeOut();
+    event.preventDefault();
+  });
 
+  $navFigures.click(function(event) {
+    $(this).addClass("ui--active");
+    $navMethods.removeClass("ui--active");
+    $sectionFigures.fadeIn();
+    $sectionMethods.fadeOut();
+    event.preventDefault();
+  });
+});
